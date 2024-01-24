59aa7278d46cdf970791c691c65dc7ee7e3e2c00
webworld420 <76555387+webworld420@users.noreply.github.com>
2024-01-24 11:10:40 +0600

    error page update

--- Sathee-React/src/CommonComponents/404/Error.jsx
+++ Sathee-React/src/CommonComponents/404/Error.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Error() {
   return (
-    <div>Error</div>
+    <div className='error-page'>
+      <h6 className='text-center'>Sorry we couldn't find the page, please try again...</h6>
+    </div>
   )
 }
